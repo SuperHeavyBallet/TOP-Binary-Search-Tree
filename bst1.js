336ded7cff5d6b8202c30855ab3964417f8f2dba
Alexander Gorham <agram@hotmail.co.uk>
2024-01-20 04:30:13 +0100

    Further progress

--- bst1.js
+++ bst1.js
@@ -1,10 +1,10 @@
+/// HTML DECLARATIONS ///
 const body = document.getElementById("body");
-
 const displayText = document.createElement("h1");
-
 displayText.textContent = "Hello World!";
 body.appendChild(displayText);
 
+/// INDIVIDUAL NODE CLASS ///
 class Node
 {
     constructor(value, left, right)
@@ -13,16 +13,30 @@ class Node
         this.left = left;
         this.right = right;
     }
+    /*
+    Each Node is:
+    ---V---
+    --/-\--
+    -L---R-
+    */
     
 }
 
+/// TREE CLASS ///
 class Tree
 {
+
+    // Generate the tree root from an input Array //
     constructor(array)
     {
+        this.elementCounter = 0;
         this.root = this.buildTree(this.removeDuplicates(array), 0, array.length - 1);
+        
     }
 
+
+
+    // Recursive Method to output the Tree Nodes/Values into an HTML DOM
     display(currentNode, previousNode, direction)
     {
         const thisNode = currentNode;
@@ -39,12 +53,12 @@ class Tree
             body.appendChild(newDOMNode);
             body.appendChild(prevDOMNode);
 
-            if (thisNode.left && thisNode.left.value != undefined)
+            if (thisNode.left)
             {
                 this.display(thisNode.left, thisNode, "Left");
             }
 
-            if (thisNode.right && thisNode.right.value != undefined)
+            if (thisNode.right)
             {
                 this.display(thisNode.right, thisNode, "Right");
             }
@@ -59,13 +73,15 @@ class Tree
 
     }
 
+    // Convert the input Array to a Set of unique values, and then return that Set into a new Array
     removeDuplicates(inputArray)
     {   
         const uniqueSet = new Set(inputArray);
         const uniqueArray = Array.from(uniqueSet);
-        return(uniqueArray);
+        return this.sortArray(uniqueArray);
     }
 
+    // Sort the now Unique array in order from smallest to largest value elements
     sortArray(inputArray)
     {
         if (inputArray.length <= 1)
@@ -73,13 +89,17 @@ class Tree
             return inputArray;
         }
 
+        // Find the middle Index by cutting the array in half, 
+        // set left and right as the remaining array to either side of that middle index
         const middleIndex = Math.floor(inputArray.length / 2);
         const leftHalf = inputArray.slice(0, middleIndex);
         const rightHalf = inputArray.slice(middleIndex);
 
-        const sortedLeft = sortArray(leftHalf);
-        const sortedRight = sortArray(rightHalf);
+        // Create variables by repeating this method with the split left and right half
+        const sortedLeft = this.sortArray(leftHalf);
+        const sortedRight = this.sortArray(rightHalf);
 
+        // Merge the left and right half variables
         return this.merge(sortedLeft, sortedRight);
     }
 
@@ -89,6 +109,10 @@ class Tree
         let leftIndex = 0;
         let rightIndex = 0;
 
+        // While left index is less than the length of the left array, and right index is less than the length of the right array
+        // If the Value in the left array at the current Left Index is less than the equivelant in right
+        // Push that smaller value into the result array and increase the left index count by one
+        // If not, push the right side into the result value and increase the right index count by one
         while (leftIndex < left.length && rightIndex < right.length)
         {
             if (left[leftIndex] < right[rightIndex])
@@ -103,117 +127,108 @@ class Tree
             }
         }
 
+        // Return the ordered Result array, 
+        // also concatenate the remaining values in the left and right after the last index increase, in case any remain which are already in order
         return result.concat(left.slice(leftIndex), right.slice(rightIndex));
     }
 
+
+    // Recursive Method to build the tree from an input array (This class by default uses the initial input array)
+    // Arguments are the array, array start and end point
     buildTree(inputArray, start, end)
     {
+        // If the start is greater than the end (end will normally be array.length -1, 
+        // so array of 1 element would be start: 0, end: 1-1 = 0, so this would still return a single node
+        // array of 0 elements would be start: 0, end: 0-1 = -1, so this would return null)
+
+        this.elementCounter ++;
+        //console.log(this.elementCounter);
 
         if (start > end)
         {
             return null;
         }
 
-        let middleIndex = parseInt((start + end) / 2);
-
+        // Get middle index point by adding 0 or other argument value + array.length and dividing by two, 
+        // using Math.floor to round down in case of decimals
+        let middleIndex = Math.floor((start + end) / 2);
+        console.log(middleIndex);
+        // Create a new node with the value of the array element at the middle index point
         const node = new Node(inputArray[middleIndex]);
 
+        // Create a left and right child node using this same method, but with the array split to either side of the middle index point
         node.left = this.buildTree(inputArray, start, middleIndex -1);
         node.right = this.buildTree(inputArray, middleIndex +1, end);
-
+    
         return node;
+        
+    }
+
+    // Method to pass through each tree node until a suitable position is found to place the number chosen
+    traverseTreeToInsert( currentNode, insertNumber, previousNode)
+    {
+
+        if (currentNode)
+        {
+            console.log(`Current: ${currentNode.value}`);
+            console.log(`Insert: ${insertNumber}`);
+
+            if (currentNode.value === insertNumber)
+            {
+                console.log("++ Already in the tree ++");
+                return;
+            }
+
+            if (currentNode.value < insertNumber)
+            {
+                console.log(">> Go Right >>");
+                console.log(currentNode);
+                this.traverseTreeToInsert(currentNode.right, insertNumber, currentNode);
+            }
+            else if (currentNode.value > insertNumber)
+            {
+                console.log("<< Go Left <<");
+                console.log(currentNode);
+                this.traverseTreeToInsert(currentNode.left, insertNumber, currentNode);
+            }
+        }
+        else{
+            console.log("!! Empty Node !!");
+
+            if (!currentNode)
+            {
+
+                if (previousNode.value < insertNumber)
+                {
+                    const newNode = new Node(insertNumber);
+                    previousNode.right = newNode;
+                    console.log(`New Node: ${previousNode.right.value}`);
+                }
+
+                else if (previousNode.value > insertNumber)
+                {
+                    const newNode = new Node(insertNumber);
+                    previousNode.left = newNode;
+                    console.log(`New Node: ${previousNode.left.value}`);
+                }
+                
+            }
+
+        }
+
+        this.display(tree.root, 0, "Root");
+
         
 
     }
 
-    getTreeHeight(){
-        let lengthToLeft = 0;
-        let lengthToRight = 0;
-        let pointerToLeft = this.root.left;
-        let pointerToRight = this.root.right;
-        while (pointerToLeft != null)
-        {
-            ++lengthToLeft;
-            pointerToLeft = pointerToLeft.left;
-        }
-        while (pointerToRight != null)
-        {
-            ++lengthToRight;
-            pointerToRight = pointerToRight.right;
-        }
-        const length = lengthToRight > lengthToLeft ?
-            lengthToRight : lengthToLeft;
-
-            // add 1 for the root
-            return length + 1;
-    }
-
-
-
-
 }
 
 
-const unsortedArray =  [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-const tree = new Tree(unsortedArray);
 
-const numberToInsert = 6;
 
-function traverseTreeToInsert( currentNode, insertNumber, previousNode)
-{
 
-    if (currentNode)
-    {
-        console.log(`Current: ${currentNode.value}`);
-        console.log(`Insert: ${insertNumber}`);
-
-        if (currentNode.value === insertNumber)
-        {
-            console.log("++ Already in the tree ++");
-            return;
-        }
-
-        if (currentNode.value < insertNumber)
-        {
-            console.log(">> Go Right >>");
-            console.log(currentNode);
-            traverseTreeToInsert(currentNode.right, insertNumber, currentNode);
-        }
-        else if (currentNode.value > insertNumber)
-        {
-            console.log("<< Go Left <<");
-            console.log(currentNode);
-            traverseTreeToInsert(currentNode.left, insertNumber, currentNode);
-        }
-    }
-    else{
-        console.log("!! Empty Node !!");
-
-        if (!currentNode)
-        {
-
-            if (previousNode.value < insertNumber)
-            {
-                const newNode = new Node(insertNumber);
-                previousNode.right = newNode;
-                console.log(`New Node: ${previousNode.right.value}`);
-            }
-
-            else if (previousNode.value > insertNumber)
-            {
-                const newNode = new Node(insertNumber);
-                previousNode.left = newNode;
-                console.log(`New Node: ${previousNode.left.value}`);
-            }
-            
-        }
-
-    }
-
-    
-
-}
 
 function traverseTreeToDelete(currentNode, deleteNumber, previousNode)
 {
@@ -233,13 +248,13 @@ function traverseTreeToDelete(currentNode, deleteNumber, previousNode)
         {
             console.log(">> Go Right >>");
             console.log(currentNode);
-            traverseTreeToInsert(currentNode.right, deleteNumber, currentNode);
+            traverseTreeToDelete(currentNode.right, deleteNumber, currentNode);
         }
         else if (currentNode.value > deleteNumber)
         {
             console.log("<< Go Left <<");
             console.log(currentNode);
-            traverseTreeToInsert(currentNode.left, deleteNumber, currentNode);
+            traverseTreeToDelete(currentNode.left, deleteNumber, currentNode);
         }
     }
     else{
@@ -268,23 +283,22 @@ function traverseTreeToDelete(currentNode, deleteNumber, previousNode)
 
 }
 
-function preOrder(node, nodeName)
-{
-    if (node == null)
-    {
-        return;
-    }
-    console.log(`${nodeName}: `, node.value);
-    preOrder(node.left, "Left Child");
-    preOrder(node.right, "Right Child");
-}
+
+
+const unsortedArray =  [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 72, 46, 99, 1054, 123, 53, 96, 83, 62];
+
+
+const tree = new Tree(unsortedArray);
+console.log(tree.removeDuplicates(unsortedArray));
+tree.display(tree.root, 0, "Root");
+
+const numberToInsert = 6;
 
 const numberToDelete = 7;
 
+//tree.traverseTreeToInsert(tree.root, numberToInsert);
 
 
-tree.display(tree.root, 0, "Root");
-console.log(tree.getTreeHeight());
 
 
 
